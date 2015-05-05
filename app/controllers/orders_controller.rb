11c6a835c85965f53cd2fa0c28abfccf2e897c76
Ann Chen <annchen@andrew.cmu.edu>
2015-05-05 15:22:53 -0400

    orders are working now yayay

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   include BreadExpressHelpers::Cart
+    include BreadExpressHelpers::Shipping
+
 
 
   before_action :check_login
@@ -7,17 +9,19 @@ class OrdersController < ApplicationController
   authorize_resource
   
   def index
+
     if logged_in? && !current_user.role?(:customer)
       @pending_orders = Order.not_shipped.chronological.paginate(:page => params[:page]).per_page(5)
       @all_orders = Order.chronological.paginate(:page => params[:page]).per_page(5)
     else
-      #need this to work after i finish cart stuff
+
       @pending_orders = current_user.customer.orders.not_shipped.chronological.paginate(:page => params[:page]).per_page(5)
       @all_orders = current_user.customer.orders.chronological.paginate(:page => params[:page]).per_page(5)
     end 
   end
 
   def show
+
     @order_items = @order.order_items.to_a
     if current_user.role?(:customer)
       @previous_orders = current_user.customer.orders.chronological.to_a
@@ -27,12 +31,18 @@ class OrdersController < ApplicationController
   end
 
   def new
-  @order = Order.new()
+  @order = Order.new
   end
 
   def create
     @order = Order.new(order_params)
     @order.date = Date.today
+
+    if current_user.role?(:customer)
+    @order.customer_id = current_user.customer.id
+    end
+
+    @order.grand_total = calculate_cart_items_cost + calculate_cart_shipping
 
 
     if @order.save
