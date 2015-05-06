f856b1640797f4d27b33f463e71c9cd9070315ef
Ann Chen <annchen@andrew.cmu.edu>
2015-05-06 10:29:29 -0400

    fiddled with persmisions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,27 +8,73 @@ class Ability
     if user.role? :admin
 
       can :manage, :all
+
     elsif user.role? :customer
-      can :edit, OrderItem
-      can :create, Order
       can :create, Customer
-      can :edit, Customer
-      can :manage, Address
-      can :read, :all
-      can :create, User
+
+
+
+      can :update, Customer do |c|
+      c.id = user.customer_id
+      end
+       can :show, Customer do |c|
+        c.id = user.customer.id
+      end
+
+ can :show, User do |c|
+      c.id = user.customer_id
+      end
+       can :show, User do |c|
+        c.id = user.id
+      end
+
+
+
+      can :create, Address
+      can :edit, Address
+      can :read, Address
+
+      can :read, Item
+      can :index, Item
+
+      can :read, OrderItem
+      can :create, OrderItem
+
+      can :read, Order
+
       can :add_to_cart, Item
       can :remove_from_cart, Item
 
+
     elsif user.role? :baker
-      can :read, :all
+      can :read, OrderItem
+      can :read, Order
+
+      can :read, Item
+      can :show, Item
+
+      
+
+      can :read, Address
+      can :edit, Address
+      can :create, Address
       can :edit, OrderItem
 
      elsif user.role? :shipper
-     can :read, :all
-     can :manage, :all
+     can :read, OrderItem
+     can :read, Item
+     can :show, Item
+     can :read, Order
+
+     can :manage, Address
+
      can :ship_item, OrderItem 
+    
+
     else 
-      can :read, :all
+      can :read, Home
+      can :read, Item
+
    
 
     end
